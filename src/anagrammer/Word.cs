d714b491de4311524d5e3c968cbf3e2d568bd3a2
Craig Boucher <craig@craigboucher.com>
2020-11-20 22:37:14 -0600

    Initial word

---
+++ src/anagrammer/Word.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace anagrammer
+{
+    public record Word
+    {
+        public string Value { get; init; }
+        public IReadOnlyDictionary<char, int> LetterGrouping { get; init; }
+        public Word(string value)
+        {
+            Value = value;
+            LetterGrouping = value.GroupBy(letter => letter).ToDictionary(g => g.Key, g => g.Count());
+        }
+    }
+}
